a3c7cd4d1127fbd64c507f558dff0884dc1a0b5d
Ryan White <mrwhiterk@gmail.com>
2019-12-09 19:22:47 -0500

    just added jwt signup and login with auth.js and no passport

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 let mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
-
-console.log('hit user model')
+const jwt = require('jsonwebtoken')
 
 let userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
@@ -11,15 +10,53 @@ let userSchema = new mongoose.Schema({
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Order'
     }
-  ]
+  ],
+  tokens: [{
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
 
-userSchema.pre('save', async function (next) {
-  const user = this;
+
+userSchema.pre('save', async function(next) {
+  const user = this
   if (user.isModified('password')) {
     user.password = await bcrypt.hash(user.password, 8)
   }
   next()
 })
 
-module.exports = mongoose.model('User', userSchema)
+userSchema.methods.generateAuthToken = async function() {
+  const user = this
+  
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.SECRET_KEY)
+
+  user.tokens = user.tokens.concat({
+    token
+  })
+
+  await user.save()
+  
+  return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new Error('Unable to login')
+  }
+  
+  const isMatch = await bcrypt.compare(password, user.password)
+  
+  if (!isMatch) {
+    throw new Error('Unable to login')
+  }
+  
+  return user
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
--- routes/users.js
+++ routes/users.js
@@ -3,92 +3,33 @@ var router = express.Router()
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const auth = require('../middleware/auth')
 
 /* GET users listing. */
-router.post('/login', function(req, res) {
-  let { email, password } = req.body
-
-  User.findOne({ email }, (err, user) => {
-    if (user) {
-      
-      bcrypt.compare(password, user.password, (error, result) => {
-        if (error) {
-          return res.send({ error })
-        } else {
-          if (!result) {
-            const error = {
-              confirmation: false,
-              message: 'incorrect email or password',
-              status: 400
-            }
-
-            return res.send({ error })
-          } else {
-            const payload = {
-              id: user.id,
-              email: user.email
-            }
-
-            jwt.sign(
-              payload,
-              process.env.SECRET_KEY,
-              { expiresIn: 3600 },
-              (err, token) => {
-                if (error) {
-                  res.json({ error })
-                } else {
-                  let success = {
-                    confirmation: true,
-                    message: 'successfully logged in',
-                    token: `Bearer ${token}`
-                  }
-
-                  //success to login
-                  return res.send(success)
-                }
-              }
-            )
-          }
-        }
-      })
-    } else {
-      res.send({ error: 'User does not exist'})
-    }
-  })
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    const token = await user.generateAuthToken()
+    res.send({ user, token })
+  } catch (e) {
+    res.status(400).send({ e })
+  }
 })
 
-router.post('/signup', function (req, res) {
-  const newUser = new User({
-    email: req.body.email,
-    password: req.body.password
-  })
+router.post('/signup', async (req, res) => {
+  const user = new User(req.body)
 
-  newUser.save().then(user => {
-    const payload = {
-      id: user.id,
-      email: user.email
-    }
+  try {
+    await user.save()
+    const token = await user.generateAuthToken()
+    res.status(201).send({ user, token })
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
 
-    jwt.sign(
-      payload,
-      process.env.SECRET_KEY,
-      {
-        expiresIn: 3600
-      },
-      (error, token) => {
-        if (error) return res.send({ error })
-
-        let success = {
-          email: user.email,
-          confirmation: true,
-          token: `Bearer ${token}`
-        }
-
-        return res.send(success)
-      }
-    )
-  })
-  .catch(error => res.send({ error }))
+router.get('/me', auth, async (req, res) => {
+  res.send(req.user)
 })
 
 module.exports = router
