bc8886b6bee22ff5bf94cea955bf187e29ca0583
Ryan White <mrwhiterk@gmail.com>
2019-12-14 16:25:11 -0500

    got cancel order working

--- routes/orders.js
+++ routes/orders.js
@@ -30,4 +30,19 @@ router.post('/', auth, async (req, res) => {
   }
 })
 
+/* PUT cancel order. */
+router.put('/cancel/:id', auth, async (req, res) => {
+  try {
+    let order = await Order.findById(req.params.id);
+
+    order.submitted = false
+    await order.save()
+
+    res.send(order)
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error)
+  }
+})
+
 module.exports = router
