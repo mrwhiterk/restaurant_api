4c9b6eb07caf3224e6ca2251748308e2ca22619a
Ryan White <mrwhiterk@gmail.com>
2019-12-16 10:25:11 -0500

    mark incomplete btn working on dashboard

--- routes/orders.js
+++ routes/orders.js
@@ -14,6 +14,17 @@ router.get('/', auth, async (req, res) => {
   }
 })
 
+/* GET all order listing. */
+router.get('/all', auth, async (req, res) => {
+  try {
+    let data = await Order.find().populate('userId').exec();
+    console.log(data);
+    res.send(data)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
 /* POST add order. */
 router.post('/', auth, async (req, res) => {
   let newOrder = new Order({
@@ -36,6 +47,49 @@ router.put('/cancel/:id', auth, async (req, res) => {
     let order = await Order.findById(req.params.id);
 
     order.submitted = false
+    await order.save()
+
+    res.send(order)
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error)
+  }
+})
+
+router.put('/resume/:id', auth, async (req, res) => {
+  try {
+    let order = await Order.findById(req.params.id);
+
+    order.submitted = true
+    await order.save()
+
+    res.send(order)
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error)
+  }
+})
+
+router.put('/complete/:id', auth, async (req, res) => {
+  try {
+    let order = await Order.findById(req.params.id);
+
+    order.completed = true
+    await order.save()
+
+    res.send(order)
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error)
+  }
+})
+
+
+router.put('/incomplete/:id', auth, async (req, res) => {
+  try {
+    let order = await Order.findById(req.params.id);
+
+    order.completed = false
     await order.save()
 
     res.send(order)
